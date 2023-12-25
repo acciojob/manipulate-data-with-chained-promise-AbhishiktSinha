fd84881372e8f3b17574397e472d0cbe34c8301b
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-12-25 18:58:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const arr = [1, 2, 3, 4];
+const output = document.getElementById('output');
+
+new Promise (function (resolve, reject) {
+
+	setTimeout((arr)=> {
+		arr.forEach((element)=> {
+			if(Math.floor(Number(element) % 2) === 0) {
+				output.innerText += element + ',';
+			}
+		})
+	}, 1000);
+
+	setTimeout((arr)=> {
+		output.textContent = arr.map((element)=> {
+			return element * 2;
+		});
+	},2000);
+
+	resolve('done');
+}).then((result)=> {
+	console.log(result);
+})
